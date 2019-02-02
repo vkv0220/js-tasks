ea1494d490f7a30da393e5ef8d40f5d7067705cf
vkv0220 <36685410+vkv0220@users.noreply.github.com>
2019-02-02 21:51:51 +0700

    Voter finished

--- vote/script.js
+++ vote/script.js
@@ -1,16 +1,36 @@
 class Voter {
-    // ... ваш код
+
     constructor (options) {
         this._element = options.elem;
         this._render();
+        this.voteField = this._element.querySelector('.vote');
+        //this.voteUpButton = this._element.querySelector('.up');
+        this.voteDownButton = this._element.querySelector('.down');
+        this._element.addEventListener('click', this._vote.bind(this), event);
+        this._element.onmousedown = () => false;
+
     }
 
-    setVote() {
-
+    setVote(value) {
+        this.voteField.innerText = value;
     }
 
     _render() {
+        this._element.innerHTML = `
+          <span class="down">—</span>
+          <span class="vote">0</span>
+          <span class="up">+</span>`;
+    }
 
+    _vote(event) {
+        let target = event.target;
+        if (target.closest ('.up')) {
+            this.voteField.innerText++;
+        }
+        if (target === this.voteDownButton) {
+        //if (target.closest('.down')) {
+            this.voteField.innerText--;
+        }
     }
 
 }
